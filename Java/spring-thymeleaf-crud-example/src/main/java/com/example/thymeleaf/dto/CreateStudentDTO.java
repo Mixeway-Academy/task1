201066f3d28d0681147b771593aa0f60a4c257eb
Ernest Śmiech <ernest.smiech.stud@pw.edu.pl>
2023-11-12 14:51:48 +0100

    task 2 - xss fix

--- Java/spring-thymeleaf-crud-example/src/main/java/com/example/thymeleaf/dto/CreateStudentDTO.java
+++ Java/spring-thymeleaf-crud-example/src/main/java/com/example/thymeleaf/dto/CreateStudentDTO.java
@@ -4,9 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Getter
@@ -14,10 +12,13 @@ import java.time.LocalDate;
 public class CreateStudentDTO {
 
     @NotEmpty(message = "{NotEmpty.name}")
+    @Pattern(regexp = "[a-zA-Z0-9]+", message = "Enter alphanumerical for name")
+    @Size(max = 127, message = "Enter less than 127 signs")
     private String name;
 
     @Email
     @NotEmpty(message = "{NotEmpty.email}")
+    @Size(max = 127, message = "Enter less than 127 signs")
     private String email;
 
     @NotNull(message = "{NotNull.birthday}")
@@ -25,23 +26,37 @@ public class CreateStudentDTO {
     private LocalDate birthday;
 
     @NotEmpty(message = "{NotEmpty.zipCode}")
+    @Size(max = 127, message = "Enter less than 127 signs")
+    @Pattern(regexp = "[0-9]+", message = "Enter correct zip code number")
     private String zipCode;
 
     @NotEmpty(message = "{NotEmpty.street}")
+    @Size(max = 127, message = "Enter less than 127 signs")
+    @Pattern(regexp = "[a-zA-Z0-9]+", message = "Enter alphanumerical for street")
     private String street;
 
     @NotEmpty(message = "{NotEmpty.number}")
+    @Size(max = 127, message = "Enter less than 127 signs")
+    @Pattern(regexp = "[0-9]+", message = "Enter correct number")
     private String number;
 
+    @Pattern(regexp = "[a-zA-Z0-9]*", message = "Enter alphanumerical for complement")
+    @Size(max = 127, message = "Enter less than 127 signs")
     private String complement;
 
     @NotEmpty(message = "{NotEmpty.district}")
+    @Pattern(regexp = "[a-zA-Z0-9]+", message = "Enter alphanumerical for district")
+    @Size(max = 127, message = "Enter less than 127 signs")
     private String district;
 
     @NotEmpty(message = "{NotEmpty.city}")
+    @Pattern(regexp = "[a-zA-Z0-9]+", message = "Enter alphanumerical for city")
+    @Size(max = 127, message = "Enter less than 127 signs")
     private String city;
 
     @NotEmpty(message = "{NotEmpty.state}")
+    @Pattern(regexp = "[a-zA-Z0-9]+", message = "Enter alphanumerical for state")
+    @Size(max = 127, message = "Enter less than 127 signs")
     private String state;
 
 }
