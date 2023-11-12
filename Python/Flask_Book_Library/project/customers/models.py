622a2b9b57cd5e98111ec9f14a6c2f3e4993673a
Paweł Popiołek <56610497+ppopiolek@users.noreply.github.com>
2023-11-12 16:57:04 +0100

    Customers validation

--- Python/Flask_Book_Library/project/customers/models.py
+++ Python/Flask_Book_Library/project/customers/models.py
@@ -1,7 +1,6 @@
 from project import db, app
 
-
-# Customer model
+# Model Klienta
 class Customer(db.Model):
     __tablename__ = 'customers'
     id = db.Column(db.Integer, primary_key=True)
@@ -10,6 +9,20 @@ class Customer(db.Model):
     age = db.Column(db.Integer)
 
     def __init__(self, name, city, age):
+        # Walidacja nazwy
+        if not 1 <= len(name) <= 64:
+            raise ValueError("Nazwa musi mieć od 1 do 64 znaków.")
+        
+        if any(char in "!@#$%^&*()_+=[]{};:'\"<>,./?\\|" for char in name):
+            raise ValueError("Nazwa zawiera niedozwolone znaki.")
+
+        # Walidacja miasta
+        if not 1 <= len(city) <= 64:
+            raise ValueError("Nazwa miasta musi mieć od 1 do 64 znaków.")
+        
+        if any(char in "!@#$%^&*()_+=[]{};:'\"<>,./?\\|" for char in city):
+            raise ValueError("Nazwa miasta zawiera niedozwolone znaki.")
+
         self.name = name
         self.city = city
         self.age = age
